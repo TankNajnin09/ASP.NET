d01c415970e25efcbdc20115937379c0cae6129a
TankNajnin09 <tankashraf771@gmail.com>
2026-02-08 19:54:17 -0800

    Add files via upload

---
+++ Cookie_state_17/State_Form.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="State_Form.aspx.cs" Inherits="State_Form" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>ViewState and Cookies Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <!-- ViewState Section -->
+        <h3>ViewState Example</h3>
+        <asp:TextBox ID="TextBox1" runat="server" Placeholder="Enter message"></asp:TextBox><br /><br />
+        <asp:Button ID="Button1" runat="server" Text="Store in ViewState" OnClick="Button1_Click" /><br /><br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+
+        <hr />
+
+        <!-- Cookies Section -->
+        <h3>Cookies Example</h3>
+        <asp:Button ID="Button2" runat="server" Text="Create Greeting Cookie" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Text="Read Greeting Cookie" OnClick="Button3_Click" /><br /><br />
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+
+    </form>
+</body>
+</html>
---
+++ Cookie_state_17/State_Form.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class State_Form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    // ViewState button
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ViewState["Message"] = TextBox1.Text;
+        Label1.Text = "ViewState Message: " + ViewState["Message"].ToString();
+    }
+
+    // Create cookie
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        HttpCookie cookie = new HttpCookie("Greeting");
+        cookie.Value = "Hello! Welcome to ASP.NET";
+        cookie.Expires = DateTime.Now.AddDays(1);
+        Response.Cookies.Add(cookie);
+
+        Label2.Text = "Greeting cookie stored.";
+    }
+
+    // Read cookie
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        if (Request.Cookies["Greeting"] != null)
+        {
+            Label2.Text = "Greeting from Cookie: " +
+                          Request.Cookies["Greeting"].Value;
+        }
+        else
+        {
+            Label2.Text = "Cookie not found.";
+        }
+    }
+
+}
