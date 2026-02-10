6b50d1086910759fecbbd9a0f4e7d2310a5730c7
TankNajnin09 <tankashraf771@gmail.com>
2026-02-09 22:41:29 -0800

    Add files via upload

---
+++ ViewSite_Login_21/Login_Form.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login_Form.aspx.cs" Inherits="Login_Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login and ViewState Demo</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Login Section</h2>
+        Username:
+        <asp:TextBox ID="txtUser" runat="server"></asp:TextBox><br /><br />
+
+        Password:
+        <asp:TextBox ID="txtPass" runat="server" TextMode="Password"></asp:TextBox><br /><br />
+
+        <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" /><br /><br />
+        <asp:Label ID="lblLoginMsg" runat="server" ForeColor="Red"></asp:Label>
+        <hr />
+
+        <h2>ViewState Demo Section</h2>
+        Enter something:
+        <asp:TextBox ID="txtData" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" /><br /><br />
+
+        <asp:Label ID="lblResult" runat="server" ForeColor="Blue"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ViewSite_Login_21/Login_Form.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login_Form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Session["User"] != null)
+        {
+            lblLoginMsg.ForeColor = System.Drawing.Color.Green;
+            lblLoginMsg.Text = "Logged in as: " + Session["User"].ToString();
+        }
+    }
+     protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        // Simple authentication (for demo only)
+        if (txtUser.Text == "admin" && txtPass.Text == "admin")
+        {
+            Session["User"] = txtUser.Text;   // Store login in Session
+            lblLoginMsg.ForeColor = System.Drawing.Color.Green;
+            lblLoginMsg.Text = "Login Successful! Welcome " + Session["User"];
+        }
+        else
+        {
+            lblLoginMsg.ForeColor = System.Drawing.Color.Red;
+            lblLoginMsg.Text = "Invalid Username or Password!";
+        }
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        // ViewState automatically preserves txtData value
+        lblResult.Text = "You entered: " + txtData.Text;
+    }
+}
