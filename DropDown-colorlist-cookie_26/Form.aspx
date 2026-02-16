6a5832d7a13d0d5a86f15b3447a6cce641094f5d
TankNajnin09 <tankashraf771@gmail.com>
2026-02-15 19:26:03 -0800

    Add files via upload

---
+++ DropDown-colorlist-cookie_26/Form.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Form.aspx.cs" Inherits="Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Color Preference with Cookie</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlColors" runat="server">
+                <asp:ListItem Text="Red" Value="Red"></asp:ListItem>
+                <asp:ListItem Text="Green" Value="Green"></asp:ListItem>
+                <asp:ListItem Text="Blue" Value="Blue"></asp:ListItem>
+                <asp:ListItem Text="Yellow" Value="Yellow"></asp:ListItem>
+            </asp:DropDownList>
+            <asp:Button ID="btnSetColor" runat="server" Text="Set Color" OnClick="btnSetColor_Click" />
+            <br /><br />
+            <asp:Label ID="lblPreference" runat="server" Text="Your color preference will appear here." />
+        </div>
+    </form>
+</body>
+</html>
---
+++ DropDown-colorlist-cookie_26/Form.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Check if the cookie exists and display the saved color
+            HttpCookie colorCookie = Request.Cookies["FavoriteColor"];
+            if (colorCookie != null)
+            {
+                lblPreference.Text = "Your saved color preference is: " + colorCookie.Value;
+                ddlColors.SelectedValue = colorCookie.Value; // Optional: pre-select in dropdown
+            }
+        }
+    }
+
+    protected void btnSetColor_Click(object sender, EventArgs e)
+    {
+        string selectedColor = ddlColors.SelectedValue;
+
+        // Create a cookie
+        HttpCookie colorCookie = new HttpCookie("FavoriteColor");
+        colorCookie.Value = selectedColor;
+        colorCookie.Expires = DateTime.Now.AddDays(30); // Cookie expires in 30 days
+
+        Response.Cookies.Add(colorCookie);
+
+        lblPreference.Text = "Your color preference has been saved: " + selectedColor;
+    }
+}
