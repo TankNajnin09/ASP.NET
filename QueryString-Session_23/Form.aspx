92fe05ad38307731fff209e2f68328d867735bcf
TankNajnin09 <tankashraf771@gmail.com>
2026-02-15 19:09:10 -0800

    Add files via upload

---
+++ QueryString-Session_23/Form.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Form.aspx.cs" Inherits="Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Session + QueryString Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <!-- Input Section -->
+            <asp:Panel ID="pnlInput" runat="server">
+                Name:
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                <br /><br />
+
+                Age:
+                <asp:TextBox ID="txtAge" runat="server"></asp:TextBox>
+                <br /><br />
+
+                <asp:Button ID="btnSubmit" runat="server" 
+                    Text="Submit" 
+                    OnClick="btnSubmit_Click" />
+            </asp:Panel>
+
+            <br />
+
+            <!-- Result Section -->
+            <asp:Label ID="lblResult" runat="server" 
+                Font-Bold="true" 
+                ForeColor="Blue">
+            </asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ QueryString-Session_23/Form.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Check if QueryString exists
+            if (Request.QueryString["Age"] != null)
+            {
+                string name = Session["UserName"] != null
+                              ? Session["UserName"].ToString()
+                              : "Unknown";
+
+                string age = Request.QueryString["Age"];
+
+                lblResult.Text = "Name: " + name + "<br/>Age: " + age;
+
+                pnlInput.Visible = false; // Hide input after submit
+            }
+        }
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        // Store Name in Session
+        Session["UserName"] = txtName.Text;
+
+        // Redirect to same page using QueryString (Age)
+        Response.Redirect("Form.aspx?Age=" + txtAge.Text);
+    }
+
+}
