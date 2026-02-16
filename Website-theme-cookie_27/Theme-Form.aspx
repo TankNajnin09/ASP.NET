ae12274c9b8e0f529d1ac3a66dab364b847171f2
TankNajnin09 <tankashraf771@gmail.com>
2026-02-15 19:28:46 -0800

    Add files via upload

---
+++ Website-theme-cookie_27/Theme-Form.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Theme-Form.aspx.cs" Inherits="Theme_Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="UTF-8" />
+    <title>Theme Selector - ASP.NET</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        body.light-mode {
+            background-color: #ffffff;
+            color: #000000;
+        }
+
+        body.dark-mode {
+            background-color: #121212;
+            color: #ffffff;
+        }
+
+        .container {
+            margin: 50px;
+        }
+    </style>
+</head>
+<body class="<%= CurrentTheme %>">
+    <form id="form1" runat="server">
+        <div class="container">
+            <h1>Select Your Theme</h1>
+            <asp:DropDownList ID="ddlTheme" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlTheme_SelectedIndexChanged">
+                <asp:ListItem Text="Light Mode" Value="light"></asp:ListItem>
+                <asp:ListItem Text="Dark Mode" Value="dark"></asp:ListItem>
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Website-theme-cookie_27/Theme-Form.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Theme_Form : System.Web.UI.Page
+{
+    public string CurrentTheme = "light-mode"; // Default theme
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Check if theme cookie exists
+            HttpCookie themeCookie = Request.Cookies["theme"];
+            if (themeCookie != null)
+            {
+                string savedTheme = themeCookie.Value;
+                if (savedTheme == "dark")
+                    CurrentTheme = "dark-mode";
+                else
+                    CurrentTheme = "light-mode";
+
+                ddlTheme.SelectedValue = savedTheme;
+            }
+        }
+    }
+
+    protected void ddlTheme_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string selectedTheme = ddlTheme.SelectedValue;
+
+        // Set the cookie for 30 days
+        HttpCookie themeCookie = new HttpCookie("theme", selectedTheme);
+        themeCookie.Expires = DateTime.Now.AddDays(30);
+        Response.Cookies.Add(themeCookie);
+
+        // Apply the theme immediately
+        if (selectedTheme == "dark")
+            CurrentTheme = "dark-mode";
+        else
+            CurrentTheme = "light-mode";
+    }
+}
